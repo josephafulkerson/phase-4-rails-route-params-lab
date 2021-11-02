b314a81071f08b1fc41ed7584119f6812c3dc8db
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-11-02 10:51:10 -0400

    commit tree 1a490ea7d83c4cb13e340080b397028a28e96af7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,14 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name]
+      students = students.select {|student| student.first_name == params[:name] || student.last_name == params[:name] }
+    end
+    render json: students
+  end
+
+  def first
+    students = Student.find(params[:id])
     render json: students
   end
 
